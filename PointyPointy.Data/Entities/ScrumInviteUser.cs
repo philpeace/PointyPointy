26e7c484fd0a8cfd06d21bf04c6b45af563280c6
philippeace <philip.peace@drfoster.co.uk>
2015-02-03 15:28:26 +0000

    Adding ScrumInviteUser

---
+++ PointyPointy.Data/Entities/ScrumInviteUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PointyPointy.Data.Entities
+{
+    public class ScrumInviteUser
+    {
+        public int Id { get; set; }
+
+        public int ScrumInviteId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime Responded { get; set; }
+
+        public bool Accepted { get; set; }
+    }
+}
